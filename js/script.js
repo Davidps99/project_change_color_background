6f644a3572fdb7b55c8b6c2bd7a86c4b6a9a145b
David Pérez <devrap99@gmail.com>
2023-04-20 13:09:23 +0200

    Acabado

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
 
+document.querySelector("button").addEventListener("click", function() {
+
+    const randomIndice = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[randomIndice];
+
+});
